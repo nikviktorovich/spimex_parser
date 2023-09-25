88158734555fa5fd3c6b57ea6642cc85c9370351
lnfecteDru <nikitasmn98@gmail.com>
2023-09-25 18:42:50 +0300

    Рефакторинг консольного main.py

--- src/spimex_parser/apps/console/main.py
+++ src/spimex_parser/apps/console/main.py
@@ -1,62 +1,68 @@
 import datetime
 import urllib.error
 from typing import Generator
+from typing import List
 
 from spimex_parser.apps.console import database
 from spimex_parser.apps.console import deps
-from spimex_parser.modules.data_storage import repositories
-
-
-def iterate_dates(
-    start: datetime.datetime,
-) -> Generator[datetime.datetime, None, None]:
-    current_date = start
-
-    while True:
-        yield current_date
-        current_date += datetime.timedelta(days=1)
-
-
-def format_date(date: datetime.datetime) -> str:
-    return date.strftime('%Y%m%d%H%M%S')
-
-
-def get_data_file_url(formatted_date: str) -> str:
-    return f'https://spimex.com/upload/reports/oil_xls/oil_xls_{formatted_date}.xls'
-
-
-def parse_file_and_add_to_repo(
-    trading_results_repo: repositories.TradingResultsRepository,
-    data_file_path: str,
-) -> None:
-    with deps.get_parser_uow(data_file_path) as uow:
-        obtained_trading_results = uow.data.list()
-        trading_results_repo.add_bulk(obtained_trading_results)
-
-
-def parse_file_and_add_to_database(data_file_path: str) -> None:
-    with deps.get_data_uow() as uow:
-        parse_file_and_add_to_repo(uow.data, data_file_path)
-
-
-def parse_files_and_add_to_database(date: datetime.datetime) -> None:
-    formatted_date = format_date(date)
-    data_file_url = get_data_file_url(formatted_date)
-    parse_file_and_add_to_database(data_file_url)
-    print(f'Added to database contents of "{data_file_url}"')
+from spimex_parser.domain import models
 
 
 def main() -> None:
     database.create_tables()
 
     start_date = datetime.datetime(year=2023, month=1, day=1, hour=16, minute=20)
+    end_date = datetime.datetime(year=2024, month=1, day=1, hour=16, minute=20)
 
-    for current_date in iterate_dates(start_date):
+    for current_date in datetime_range(start_date, end_date):
         try:
-            parse_files_and_add_to_database(current_date)
+            add_results_from_specified_date_to_database(current_date)
         except urllib.error.HTTPError as e:
             print(f'Parsing error: "{str(e)}" (requested date: {current_date})')
-    
+
+
+def datetime_range(
+    start: datetime.datetime,
+    end: datetime.datetime,
+) -> Generator[datetime.datetime, None, None]:
+    """Генерирует последовательные даты в указанном диапазоне"""
+    current_date = start
+
+    while current_date < end:
+        yield current_date
+        current_date += datetime.timedelta(days=1)
+
+
+def add_results_from_specified_date_to_database(date: datetime.datetime) -> None:
+    """Получает данные о торгах указанной даты и добавляет в базу данных"""
+    data_file_url = get_data_file_url(date)
+    trading_results = load_results_from_file(data_file_url)
+    add_results_to_repo(trading_results)
+    print(f'Added to database contents of "{data_file_url}"')
+
+
+def get_data_file_url(date: datetime.datetime) -> str:
+    """Возвращает ссылку на файл данных указанной даты"""
+    formatted_date = format_date(date)
+    return f'https://spimex.com/upload/reports/oil_xls/oil_xls_{formatted_date}.xls'
+
+
+def format_date(date: datetime.datetime) -> str:
+    """Форматирует дату в строковое представление необходимого вида"""
+    return date.strftime('%Y%m%d%H%M%S')
+
+
+def load_results_from_file(data_file_path: str) -> List[models.TradingResult]:
+    """Загружает данные результатов торгов из указанного файла"""
+    with deps.get_parser_uow(data_file_path) as uow:
+        return uow.data.list()
+
+
+def add_results_to_repo(trading_results: List[models.TradingResult]) -> None:
+    """Добавляет указанные данные о результатах торгов в базу данных"""
+    with deps.get_data_uow() as uow:
+        uow.data.add_bulk(trading_results)
+        uow.commit()
 
 
 if __name__ == '__main__':
